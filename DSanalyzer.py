a4a858ebc7f83405c4ef3aae121551b92b199b41
Song Hu <soh516@usask.ca>
2024-05-07 19:53:25 -0600

    comment

--- DSanalyzer.py
+++ DSanalyzer.py
@@ -14,9 +14,16 @@ with open('DS.csv', 'r') as csvfile:
         x_tmp.append(row[0])  # Assuming the x-values are in the first column
         y_tmp.append(row[1])  # Assuming the y-values are in the second column
 
+# Convert a string list to float list
 y = list(map(float, y_tmp))
+
+# Convert TB to PB 
 y[:] = [tmp / 1000 for tmp in y]
+
+# Convert string to datetime object 
 x = [dt.datetime.strptime(tmp, '%m/%d/%Y').date() for tmp in x_tmp]
+
+# Plot historical data
 plt.plot(x, y, label='Historical')
 plt.xlabel('Date (Year)')
 plt.ylabel('Block usage (PB)')
@@ -25,7 +32,7 @@ plt.xticks(rotation=45)
 plt.grid(True)
 ax = plt.gca()
 ax.grid(which='major', color='#DDDDDD', linestyle='dotted', linewidth=0.8)  # Major grid lines
-ax.grid(which='minor', color='#EEEEEE', linestyle='dotted', linewidth=0.8)  # Major grid lines
+ax.grid(which='minor', color='#EEEEEE', linestyle='dotted', linewidth=0.5)  # Minor grid lines
 ax.minorticks_on()
 
 date_range = list(range(1, len(y)+1))
