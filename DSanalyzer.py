f6bf003d48e83f283ea216cb4ad0acb99c806f54
Song Hu <soh516@usask.ca>
2024-05-08 15:18:29 -0600

    do not read empty cell in csv

--- DSanalyzer.py
+++ DSanalyzer.py
@@ -8,13 +8,15 @@ import numpy as np
 x_tmp = []
 y_tmp = []
 
-with open('DS2.csv', 'r') as csvfile:
+with open('DS3.csv', 'r') as csvfile:
     csvread = csv.reader(csvfile, delimiter=',')
     # Escape header
     header = next(csvread)
     for row in csvread:
-        x_tmp.append(row[0])  # Assuming the x-values are in the first column
-        y_tmp.append(row[1])  # Assuming the y-values are in the second column
+        if row[0]:
+            x_tmp.append(row[0])  # Assuming the x-values are in the first column
+        if row[1]:
+            y_tmp.append(row[1])  # Assuming the y-values are in the second column
 
 # Convert a string list to float list
 y = list(map(float, y_tmp))
